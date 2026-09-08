8e2af3544f1a1cb3009ed2115cf99c949f3ac4d7
Lê Minh Dương <Duongwitri.0912@gmail.com>
2026-09-08 21:59:23 +0700

    Initial commit - Setup DSA Project

---
+++ src/edu/princeton/cs/algs4/QuickFindUF.java
@@ -0,0 +1,116 @@
+package edu.princeton.cs.algs4;
+
+import java.util.Scanner;
+
+public class QuickFindUF {
+    private int[] id;    // id[i] = component identifier of i
+    private int count;   // number of components
+
+    /**
+     * Initializes an empty union-find data structure with {@code n} elements.
+     *
+     * @param n the number of elements
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public QuickFindUF(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++)
+            id[i] = i;
+    }
+
+    /**
+     * Returns the number of sets.
+     *
+     * @return the number of sets (between {@code 1} and {@code n})
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns the canonical element of the set containing element {@code p}.
+     *
+     * @param p an element
+     * @return the canonical element of the set containing {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    public int find(int p) {
+        validate(p);
+        return id[p];
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n - 1));
+        }
+    }
+
+    /**
+     * Returns true if the two elements are in the same set.
+     *
+     * @param p one element
+     * @param q the other element
+     * @return {@code true} if {@code p} and {@code q} are in the same set;
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * @deprecated Replace by {@link #connected(int, int)}.
+     */
+    @Deprecated
+    public boolean connected(int p, int q) {
+        validate(p);
+        validate(q);
+        return id[p] == id[q];
+    }
+
+    /**
+     * Merges the set containing element {@code p} with the set
+     * containing element {@code q}.
+     *
+     * @param p one element
+     * @param q the other element
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        validate(p);
+        validate(q);
+        int pID = id[p];   // needed for correctness
+        int qID = id[q];   // needed for correctness
+
+        // p and q are already in the same component
+        if (pID == qID) return;
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pID) id[i] = qID;
+        }
+        count--;
+    }
+
+    /**
+     * Reads an integer n and a sequence of pairs of integers (between 0 and n-1) from standard input,
+     * where each pair represents the connection between two elements;
+     * prints the pair if the elements were not yet connected and merges the sets;
+     * and prints the number of components remaining.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextInt()) return;
+        int n = scanner.nextInt();
+        QuickFindUF uf = new QuickFindUF(n);
+        while (scanner.hasNextInt()) {
+            int p = scanner.nextInt();
+            int q = scanner.nextInt();
+            if (uf.find(p) == uf.find(q)) continue;
+            uf.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        System.out.println(uf.count() + " components");
+        scanner.close();
+    }
+}
